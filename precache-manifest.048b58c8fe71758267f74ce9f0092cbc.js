4f834346e2076bf71393b3b04f191271bdf6b450
navydrum <navydrum@gmail.com>
2020-03-03 15:02:40 +0900

    Updates

---
+++ precache-manifest.048b58c8fe71758267f74ce9f0092cbc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf9a863ceeafcdc52195cc064234be2",
+    "url": "/pr/index.html"
+  },
+  {
+    "revision": "f8d63aac1768b7a38e9b",
+    "url": "/pr/static/css/main.32077b26.chunk.css"
+  },
+  {
+    "revision": "c435d5d377a0815f5152",
+    "url": "/pr/static/js/2.82b7fa12.chunk.js"
+  },
+  {
+    "revision": "280b48dbc3281f342da16f66e41cf46c",
+    "url": "/pr/static/js/2.82b7fa12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d63aac1768b7a38e9b",
+    "url": "/pr/static/js/main.b3189125.chunk.js"
+  },
+  {
+    "revision": "6bb8ff66ffd2a49d8465",
+    "url": "/pr/static/js/runtime-main.7de7db3c.js"
+  },
+  {
+    "revision": "38fb1e86b1f5004024f773b3fc05948d",
+    "url": "/pr/static/media/champion.38fb1e86.png"
+  },
+  {
+    "revision": "9b4fd0eba67c52e74b5934b9fa86fd3c",
+    "url": "/pr/static/media/noIcon.9b4fd0eb.jpg"
+  },
+  {
+    "revision": "180877e88c620e67aa9084c8343919f0",
+    "url": "/pr/static/media/silver_3.180877e8.png"
+  }
+]);
